84a7cc888c40896b3b0168bbd37afabfe8e230b2
Jefferson Estevam <jefferson.estevam@idealizza.com.br>
2019-02-10 11:26:11 -0300

    removido avatar

--- resources/views/vendor/voyager/users/edit-add.blade.php
+++ resources/views/vendor/voyager/users/edit-add.blade.php
@@ -105,16 +105,17 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="panel panel panel-bordered panel-warning">
-                        <div class="panel-body">
-                            <div class="form-group">
-                                @if(isset($dataTypeContent->avatar))
-                                    <img src="{{ filter_var($dataTypeContent->avatar, FILTER_VALIDATE_URL) ? $dataTypeContent->avatar : Voyager::image( $dataTypeContent->avatar ) }}" style="width:200px; height:auto; clear:both; display:block; padding:2px; border:1px solid #ddd; margin-bottom:10px;" />
-                                @endif
-                                <input type="file" data-name="avatar" name="avatar">
-                            </div>
-                        </div>
-                    </div>
+                    {{--REMOVIDO AVATAR--}}
+                    {{--<div class="panel panel panel-bordered panel-warning">--}}
+                        {{--<div class="panel-body">--}}
+                            {{--<div class="form-group">--}}
+                                {{--@if(isset($dataTypeContent->avatar))--}}
+                                    {{--<img src="{{ filter_var($dataTypeContent->avatar, FILTER_VALIDATE_URL) ? $dataTypeContent->avatar : Voyager::image( $dataTypeContent->avatar ) }}" style="width:200px; height:auto; clear:both; display:block; padding:2px; border:1px solid #ddd; margin-bottom:10px;" />--}}
+                                {{--@endif--}}
+                                {{--<input type="file" data-name="avatar" name="avatar">--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
                 </div>
             </div>
 
